afda9e7bf8df7a0fb79afc420c73598b61bdad85
Matt <mgstankov30@gmail.com>
2022-06-30 13:12:31 -0400

    load subset of stores and select using the where class

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,19 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Surrey = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+
+# find all users named David who are Code Artists and sort by created_at in reverse chronological order
+@mens_stores = Store.where(mens_apparel: true, womens_apparel: false).each do |p|
+  p.name
+  p.annual_revenue
+end
+
+@womens_stores = Store.where(mens_apparel: false, womens_apparel: true).each do |p|
+  p.name
+  p.annual_revenue
+end
+
